d72db79a69a4a90cbc7b0995ccb0ad4f9d11aca2
Adam Mustapha <matawalle4u@gmail.com>
2020-04-10 20:05:30 +0100

    Test 2 script added

---
+++ test2.php
@@ -0,0 +1,7 @@
+<?php
+                                    $f1 = $_POST['field_1'];
+                                    $f2 = $_POST['field_2'];
+
+                                    $files = $_FILES['file'];
+                                    print_r($files);
+                                  ?>
